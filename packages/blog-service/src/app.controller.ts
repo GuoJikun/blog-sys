5863a8163707ea5d1a561ab7120d3b2234bc3f90
GuoJikun <guojikun486546@163.com>
2022-09-15 18:14:29 +0800

    feat: 增加模板引擎

--- packages/blog-service/src/app.controller.ts
+++ packages/blog-service/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Render, Version } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -6,7 +6,20 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
+    @Version("1")
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get("/index")
+    @Render("index")
+    root() {
+        return { message: "Hello world!" };
+    }
+
+    @Get()
+    @Version("2")
+    getHelloV2(): string {
+        return "v2 版本";
     }
 }
--- packages/blog-service/src/main.ts
+++ packages/blog-service/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
+import { join } from "node:path";
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.setBaseViewsDir(join(__dirname, "..", "views"));
+    app.setViewEngine("hbs");
+    app.enableVersioning({
+        type: VersioningType.URI,
+    });
     await app.listen(3000);
 }
 bootstrap();
